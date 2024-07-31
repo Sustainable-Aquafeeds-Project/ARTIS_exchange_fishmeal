27276c1cf21eb3ec0737f010369bf4f1c1b867d6
gclawson1 <clawsonsa1101@gmail.com>
2024-07-31 23:51:09 +0000

    adding code from first day of ARTIS exchange

---
+++ R/directories.R
@@ -0,0 +1,12 @@
+#rdsi storage
+options(scipen = 999)
+
+rdsi_dir <- "/mnt/rdsi"
+
+rdsi_raw_data_dir <- "/mnt/rdsi/raw_data"
+
+# artis data
+artis_dir <- "/mnt/rdsi/raw_data/ARTIS"
+
+#FAO data directories
+fao_dir <- "/mnt/rdsi/raw_data/fao"
